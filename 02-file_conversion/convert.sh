2ff6b2a941c707169c148a7036565f21f3aa591b
Layton Ashmore <philip.ashmore@wsu.edu>
2024-03-19 14:32:35 -0700

    updated config files, includes centroiding

--- 02-file_conversion/convert.sh
+++ 02-file_conversion/convert.sh
@@ -5,6 +5,6 @@
 # and outputs converted files there for user convenience. All raw data (.D files) should go in the 01-raw_data folder and all converted files
 # should appear in the 02-file_conversion folder
 #"peakPicking filter" provides centroiding of profile data for use with metaboigniter
-docker run --rm -e WINEDEBUG=-all -v $PWD/../01-raw_data:/data -v $PWD:/output chambm/pwiz-skyline-i-agree-to-the-vendor-licenses wine msconvert /data/*.D -o /output/ --filter "peakPicking true 1-"
+docker run --rm -e WINEDEBUG=-all -v $PWD/../01-raw_data:/data -v $PWD:/output chambm/pwiz-skyline-i-agree-to-the-vendor-licenses wine msconvert /data/*.D -o /output/
 
 
