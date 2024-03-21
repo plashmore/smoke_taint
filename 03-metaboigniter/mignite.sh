107c767e2be091e55243bcceb7252bebb8a7b201
Layton Ashmore <philip.ashmore@wsu.edu>
2024-03-21 14:00:30 -0700

    added metaboigniter fixes

--- 03-metaboigniter/mignite.sh
+++ 03-metaboigniter/mignite.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-nextflow run nf-core/metaboigniter -profile docker -resume
+#nextflow run nf-core/metaboigniter -profile docker -resume
+
+nextflow run /home/laytox/projects/metaboigniter -profile docker -resume
